b9264fc4fc296830ef90849f863559c0d4333509
chiefastro <chiefastro@gmail.com>
2020-06-29 23:19:01 -0400

    Explicitly require dependencies

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chiefastro/wp-auto-taxonomy-models",
-    packages=setuptools.find_packages(),
+    packages=[
+        'pandas', 'numpy', 'fuzzywuzzy', 'gensim', 'scipy', 'scikit-learn'
+    ],#setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
