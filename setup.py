b6182b4e668547f9398e4dd59a2445bcc25b8cce
chiefastro <chiefastro@gmail.com>
2021-07-13 22:54:11 -0400

    deps

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/chiefastro/wp-auto-taxonomy-models",
     packages=setuptools.find_packages(),
     install_requires=[
-        'pandas', 'numpy', 'fuzzywuzzy', 'scipy', 'scikit-learn'
+        'pandas', 'numpy', 'fuzzywuzzy', 'scipy', 'scikit-learn', 'yake', 'bs4'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
