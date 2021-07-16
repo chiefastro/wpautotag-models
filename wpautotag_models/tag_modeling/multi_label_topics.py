d997dc6b8d6e007f6f573971bb4fe195bc02847f
chiefastro <chiefastro@gmail.com>
2021-07-16 00:14:21 -0400

    imports

--- wpautotag_models/tag_modeling/multi_label_topics.py
+++ wpautotag_models/tag_modeling/multi_label_topics.py
@@ -1,6 +1,9 @@
 import os
 import pickle
 
+import numpy as np
+import pandas as pd
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.linear_model import LogisticRegression
